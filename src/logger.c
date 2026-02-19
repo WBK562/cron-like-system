103842ceb7b472cf490fd7ef47ef89ba3a23f124
WBK562 <olo.mieszczankowski@gmail.com>
2026-02-19 15:23:58 +0100

    Refactor logger to use current_dump_val instead of queue

--- src/logger.c
+++ src/logger.c
@@ -10,11 +10,13 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-#define QUEUE_SIZE 128
-
+// Shared variables modified by signal handlers must be volatile sig_atomic_t
 static volatile sig_atomic_t logging_state = 1;
-static volatile sig_atomic_t log_curr_level = MAX;
-static volatile sig_atomic_t lib_runnig = 0;
+static volatile sig_atomic_t log_curr_level = STANDARD;
+static volatile sig_atomic_t lib_running = 0;
+
+// Variable to store the latest dump value instead of a queue
+static volatile sig_atomic_t current_dump_val = 0;
 
 static sem_t dump_sem;
 static pthread_t dump_pthread;
@@ -22,14 +24,9 @@ static pthread_t dump_pthread;
 static FILE *log_file = NULL;
 static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-static int queue[QUEUE_SIZE];
-static volatile sig_atomic_t head = 0;
-static int tail = 0;
-
-
 static void handler_dump(int sig, siginfo_t *info, void *ucontext) {
-    queue[head] = info->si_value.sival_int;
-    head = (head + 1) % QUEUE_SIZE;
+    // Overwrite with the latest signal value
+    current_dump_val = info->si_value.sival_int;
     sem_post(&dump_sem);
 }
 
@@ -44,40 +41,48 @@ static void handler_log(int sig, siginfo_t *info, void *ucontext) {
 
 static void *dump_thread_func(void *arg) {
     char filename[128];
-    while(lib_runnig) {
+    while(lib_running) {
         sem_wait(&dump_sem);
 
-        while(tail != head) {
-            int data = queue[tail];
-            tail = (tail + 1) % QUEUE_SIZE;
-            snprintf(filename, sizeof(filename), "dump_%ld_val%d.txt", time(NULL), data);
-            FILE *f = fopen(filename, "w");
-            if (f) {
-                fprintf(f, "Czas: %ld\n", time(NULL));
-                fprintf(f, "Otrzymana wartosc sygnalu: %d\n", data);
-                fclose(f);
-            }
+        // Safety check: was the semaphore posted by logger_kill?
+        if(!lib_running) break;
+
+        // Copy value locally as quickly as possible to minimize overwrite risk
+        int data = current_dump_val;
+
+        snprintf(filename, sizeof(filename), "dump_%ld_val%d.txt", time(NULL), data);
+
+        time_t now = time(NULL);
+        struct tm *t = localtime(&now);
+        char time_buf[30];
+        strftime(time_buf, sizeof(time_buf), "%d-%m-%Y %H:%M:%S", t);
+
+        FILE *f = fopen(filename, "w");
+        if (f) {
+            fprintf(f, "Time: %s\n", time_buf);
+            fprintf(f, "Received signal value: %d\n", data);
+            fclose(f);
         }
-        if(!lib_runnig) break;
     }
     return NULL;
 }
 
 int logger_init(const char *filename, int sig_dump, int sig_toggle, int sig_level) {
-    if(lib_runnig) return -1;
+    if(lib_running) return -1;
 
-    log_file = fopen(filename,"a");
+    log_file = fopen(filename, "a");
     if(log_file == NULL) {
         return -2;
     }
-    lib_runnig = 1;
+    lib_running = 1;
 
-    sem_init(&dump_sem,0,0);
-    pthread_create(&dump_pthread,NULL,&dump_thread_func,NULL);
+    sem_init(&dump_sem, 0, 0);
+    pthread_create(&dump_pthread, NULL, &dump_thread_func, NULL);
 
     struct sigaction sa;
     sigemptyset(&sa.sa_mask);
 
+    // SA_SIGINFO flag is required to read sival_int
     sa.sa_flags = SA_SIGINFO | SA_RESTART;
 
     sa.sa_sigaction = handler_dump;
@@ -88,16 +93,18 @@ int logger_init(const char *filename, int sig_dump, int sig_toggle, int sig_leve
 
     sa.sa_sigaction = handler_log;
     sigaction(sig_toggle, &sa, NULL);
+
     return 0;
 }
 
 void logger_kill() {
-    if(!lib_runnig) return;
+    if(!lib_running) return;
 
-    lib_runnig = 0;
+    lib_running = 0;
 
+    // Post semaphore to unblock the thread and allow it to exit cleanly
     sem_post(&dump_sem);
-    pthread_join(dump_pthread,NULL);
+    pthread_join(dump_pthread, NULL);
 
     sem_destroy(&dump_sem);
     pthread_mutex_destroy(&log_mutex);
@@ -109,7 +116,7 @@ void logger_kill() {
 }
 
 void logger_write(enum log_level LogLevel, char *format, ...) {
-    if(!lib_runnig || !logging_state) return;
+    if(!lib_running || !logging_state) return;
 
     if (LogLevel > log_curr_level) return;
 
@@ -118,7 +125,7 @@ void logger_write(enum log_level LogLevel, char *format, ...) {
     char time_buf[30];
     strftime(time_buf, sizeof(time_buf), "%d-%m-%Y %H:%M:%S", t);
 
-    const char *prefix_str = "UNK";
+    const char *prefix_str;
     if (LogLevel == MIN) prefix_str = "MIN";
     if (LogLevel == STANDARD) prefix_str = "STD";
     if (LogLevel == MAX) prefix_str = "MAX";
